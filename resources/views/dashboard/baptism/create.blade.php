55bd1db136e0e4bb221ece7fce211c760915aa02
rizkyperdananst <riskiperdana345@gmail.com>
2022-12-27 01:09:55 +0700

    Bingung dah mau komen apalagi wkwkw

--- resources/views/dashboard/baptism/create.blade.php
+++ resources/views/dashboard/baptism/create.blade.php
@@ -15,8 +15,8 @@
               <label for="family_member_id">Keluarga</label>
               <select name="family_member_id" id="family_member_id" class="form-control @error('family_member_id') is-invalid @enderror" required>
                 <option>--Pilih Keluarga--</option>
-                @foreach ($family_members as $family_member)
-                  <option value="{{ $family_member->id }}">{{ $family_member->nama }}</option>
+                @foreach ($family_members as $fm)
+                  <option value="{{ $fm->id }}">{{ $fm->nama }}</option>
                 @endforeach
               </select>
               @error('family_member_id')
