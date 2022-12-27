5b367e6884cbdd3936c55bc001cd306a9bfa16e9
rizkyperdananst <riskiperdana345@gmail.com>
2022-12-27 11:40:25 +0700

    Memperbaiki

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -62,9 +62,9 @@
         <h6 class="collapse-header">Data Keluarga</h6>
         <a class="collapse-item" href="{{ route('keluarga.index') }}">Keluarga</a>
         <a class="collapse-item" href="{{ route('member.index') }}">Anggota Keluarga</a>
-        <a class="collapse-item" href="{{ route('monding.index') }}">Monding</a>
         <a class="collapse-item" href="{{ route('kawin.index') }}">Kawin</a>
         <a class="collapse-item" href="{{ route('sidi.index') }}">Sidi</a>
+        <a class="collapse-item" href="{{ route('monding.index') }}">Monding</a>
       </div>
     </div>
   </li>
