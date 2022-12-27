15e1143c567020da40702fab99824050c46db278
rizkyperdananst <riskiperdana345@gmail.com>
2022-12-27 10:57:26 +0700

    Pening

--- resources/views/dashboard/married/edit.blade.php
+++ resources/views/dashboard/married/edit.blade.php
@@ -35,12 +35,12 @@
                 <label for="kawin">Kawin</label>
                 <select name="kawin" id="kawin" value="{{ $married->kawin }}" class="form-control @error('kawin') is-invalid @enderror" required>
                   <option>--Pilih Status--</option>
-                  @foreach ($statuses as $status)
-                    @if ($status == $married->kawin)
-                      <option value="{{ $status }}" selected>{{ $status }}</option>
-                    @else
-                      <option value="{{ $status }}">{{ $status }}</option>
-                    @endif                     
+                  @foreach ($statuses as $status) 
+                  @if ($status == $married->kawin)  
+                    <option value="{{ $status }}" selected>{{ $status }}</option>
+                  @else
+                    <option value="{{ $status }}">{{ $status }}</option>
+                  @endif                     
                   @endforeach
                 </select>
               </div>
