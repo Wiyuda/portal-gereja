2bf6b95c552b88ec02b54715d04cce7ca312d1ff
rizkyperdananst <riskiperdana345@gmail.com>
2023-01-16 12:58:11 +0700

    Membuat Fix Dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Family;
+use App\Models\News;
+use App\Models\Priest;
+use App\Models\Sector;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.dashboard');
+        $family = Family::all()->count();
+        $friest = Priest::first()->count();
+        $news = News::first()->count();
+        $sector = Sector::first()->count();
+        return view('dashboard.dashboard', compact('family', 'friest', 'news', 'sector'));
     }
 }
